1478261e41b6b2a76506ae1e6a411a13f7ebb1a7
Joel Mackenzie <marihonjahmackenzie@gmail.com>
2018-12-11 13:32:28 +0300

    add callback at scripts.js for functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 function contact(first, last) {
   this.firstName = first;
   this.lastName = last;
-}
+  $(document).ready(function() {
+        $("form#new-contact").submit(function(event) {
+            event.preventDefault();
+          }
+
+        }
+      }
